074f050077d71dceaa3a276209ce946c86aa5fd1
Muhammad Hanzla <146532605+hanzlikhan@users.noreply.github.com>
2024-08-14 22:47:24 +0500

    todo app ver 03

---
+++ calculator - ver02/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import Display from './components/Display';
+import ButtonsContainer from './components/ButtonsContainer';
+import styles from './App.module.css';
+import './App.css';
+
+function App() {
+  const [calVal, setCalValue] = useState('');
+
+  const onButtonClick = (buttonText) => {
+    if(buttonText == "C"){
+      setCalValue("");
+    }else if(buttonText == "="){
+        const result = eval(calVal)
+        setCalValue(result)
+    }else{
+      const newDisplayValue = calVal + buttonText
+      setCalValue(newDisplayValue)
+    }
+    // const value = event.target.textContent; // Get the text of the clicked button
+    console.log(buttonText); // Log the value to the console
+    // Update the calculator display or perform further actions with the value here
+  };
+
+  return (
+    <div className={styles.calculator}>
+      <Display DisplayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+    </div>
+  );
+}
+
+export default App;

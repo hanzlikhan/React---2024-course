40ba0772f701f480017dba1e5d53b3536e63f179
Muhammad Hanzla <146532605+hanzlikhan@users.noreply.github.com>
2024-08-21 21:32:50 +0500

    init

---
+++ LoginForm/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LoginForm from './components/LoginForm';
+function App() {
+
+  return (
+    <>
+      <h1>Login Form</h1>
+      <LoginForm/>
+    </>
+  )
+}
+
+export default App

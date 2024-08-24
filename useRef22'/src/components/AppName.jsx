4d2ac94d5b83357f53a7df605827e15f03b5da7f
hanzlikhan <khangormani79@gmail.com>
2024-08-24 22:22:53 +0500

    init

--- useRef22'/src/components/AppName.jsx
+++ useRef22'/src/components/AppName.jsx
@@ -1,8 +1,8 @@
-// import React from 'react';
-// import styles from './AppName.module.css'; // Assuming you have CSS modules
+import React from "react";
+import styles from "./AppName.module.css"; // Assuming you have CSS modules
 
-// function AppName() {
-//   return <h1 className={styles.todoHeading}>TODO APP</h1>;
-// }
+function AppName() {
+  return <h1 className={styles.todoHeading}>TODO APP</h1>;
+}
 
-// export default AppName;
+export default AppName;

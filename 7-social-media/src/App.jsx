6b449350d2bd22b42b0cb22cca0e7c2ff4403c0a
hanzlikhan <khangormani79@gmail.com>
2024-09-01 06:59:17 +0500

    init

--- 7-social-media/src/App.jsx
+++ 7-social-media/src/App.jsx
@@ -11,25 +11,25 @@ import PostListProvider from "./store/post-list-store";
 function App() {
   const [selectedTab, setSelectedTab] = useState("Home");
 
-  // return (
-  //   <PostListProvider>
-  //     <div className="app-container">
-  //       <Sidebar
-  //         selectedTab={selectedTab}
-  //         setSelectedTab={setSelectedTab}
-  //       ></Sidebar>
-  //       <div className="content">
-  //         <Header></Header>
-  //         {selectedTab === "Home" ? (
-  //           <PostList></PostList>
-  //         ) : (
-  //           <CreatePost></CreatePost>
-  //         )}
-  //         <Footer></Footer>
-  //       </div>
-  //     </div>
-  //   </PostListProvider>
-  // );
+  return (
+    <PostListProvider>
+      <div className="app-container">
+        <Sidebar
+          selectedTab={selectedTab}
+          setSelectedTab={setSelectedTab}
+        ></Sidebar>
+        <div className="content">
+          <Header></Header>
+          {selectedTab === "Home" ? (
+            <PostList></PostList>
+          ) : (
+            <CreatePost></CreatePost>
+          )}
+          <Footer></Footer>
+        </div>
+      </div>
+    </PostListProvider>
+  );
 }
 
 export default App;

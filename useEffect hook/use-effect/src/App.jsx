392654a990d015a616e2cab97a37aa99a6ef3532
hanzlikhan <khangormani79@gmail.com>
2024-09-12 22:29:34 +0500

    init

---
+++ useEffect hook/use-effect/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar"; // Correct import path
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState(0);
+
+  useEffect(() => {
+    alert("re-render due to color change");
+  }, [color]);
+
+  return (
+    <>
+      <div>
+        <Navbar /> {/* Use the Navbar component */}
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;

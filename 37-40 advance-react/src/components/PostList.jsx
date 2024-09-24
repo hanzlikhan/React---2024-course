a7df66cc224f7013133cfc8df6688d8791378589
hanzlikhan <khangormani79@gmail.com>
2024-09-24 21:52:38 +0500

    unnit

---
+++ 37-40 advance-react/src/components/PostList.jsx
@@ -0,0 +1,43 @@
+import { useContext, useEffect, useState } from "react";
+import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
+
+const PostList = () => {
+  const { postList, addInitialPosts } = useContext(PostListData);
+  const [fetching, setFetching] = useState(false);
+
+  useEffect(() => {
+    setFetching(true);
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch("https://dummyjson.com/posts", { signal })
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+        setFetching(false);
+      })
+      .catch((error) => {
+        if (error.name !== "AbortError") {
+          console.error("Error fetching posts:", error);
+        }
+        setFetching(false); // Stop the loading spinner even if there is an error
+      });
+
+    return () => {
+      console.log("Cleaning up UseEffect.");
+      controller.abort(); // Abort request if the component unmounts
+    };
+  }, []);
+  return (
+    <>
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
+    </>
+  );
+};
+
+export default PostList;

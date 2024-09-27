5f88b7fc5ee4432e61d520bb85187d05ae29ceec
hanzlikhan <khangormani79@gmail.com>
2024-09-27 21:31:29 +0500

    init

---
+++ React-Router/react-router-dom/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My blog</h1>
+      <p>Here you can find the articles on the Various topics</p>
+    </div>
+  )
+}
+
+export default Home

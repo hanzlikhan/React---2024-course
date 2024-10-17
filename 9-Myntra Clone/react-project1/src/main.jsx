268879b6e93b5f58f3b3f73da44d7669fe3cf4a5
hanzlikhan <khangormani79@gmail.com>
2024-10-17 11:11:59 +0500

    clone react app

--- 9-Myntra Clone/react-project1/src/main.jsx
+++ 9-Myntra Clone/react-project1/src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./routes/App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Bag from "./routes/Bag.jsx";
+import { Home } from "./routes/Home.jsx";
+import hanzalStore from "./store/index.js";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />, // Corrected Home component rendering
+      },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={hanzalStore}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);

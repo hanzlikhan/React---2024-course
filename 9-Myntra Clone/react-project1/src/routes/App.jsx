ad14f017ddcf00ae5e108e51272ad0de68a5b994
hanzlikhan <khangormani79@gmail.com>
2024-10-18 10:20:07 +0500

    init

--- 9-Myntra Clone/react-project1/src/routes/App.jsx
+++ 9-Myntra Clone/react-project1/src/routes/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import { FetchItems } from "../components/FetchItems";
 
 function App() {
   return (
     <>
       <div>
         <Header />
+        <FetchItems />
         <Outlet />
         <Footer />
       </div>

8e5b0aa1cf9c6dd4aee240abc6e27d0bee97794c
hanzlikhan <khangormani79@gmail.com>
2024-10-18 10:20:17 +0500

    init

--- 9-Myntra Clone/react-project1/src/store/itemsSlice.js
+++ 9-Myntra Clone/react-project1/src/store/itemsSlice.js
@@ -1,12 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { DEFAULT_ITEMS } from "../data/items";  // Assuming this is an array of items
+
 
 const itemsSlice = createSlice({
   name: "items",
-  initialState: DEFAULT_ITEMS,  // This should directly be the array of items
+  initialState: [],  // This should directly be the array of items
   reducers: {
     addInitialItems: (state, action) => {
-      return state; // No need to modify the state if you're not adding items dynamically yet
+      return action.payload; // No need to modify the state if you're not adding items dynamically yet
     },
   },
 });

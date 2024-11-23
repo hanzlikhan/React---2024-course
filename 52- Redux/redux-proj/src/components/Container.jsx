c5d39a851e9c2616181cd4cfe822e6ca7ae592ec
hanzlikhan <khangormani79@gmail.com>
2024-11-23 12:42:10 +0500

    init

--- 52- Redux/redux-proj/src/components/Container.jsx
+++ 52- Redux/redux-proj/src/components/Container.jsx
@@ -3,10 +3,10 @@ const Container = ({ children }) => {
     <div className="card" style={{ width: "18rem" }}>
       {" "}
       {/* Fixed width style */}
-      {/* <div className="card-body">
+      <div className="card-body">
         {children} {/* Render children inside the card body */}
       </div> 
-    </div> */}
+    </div>
   );
 };
 
